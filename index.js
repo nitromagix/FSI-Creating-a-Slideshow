a08a0eb6a6796f8041d28e1c5d2cb99141706bd9
Martin <nitromagix@gmail.com>
2022-03-29 21:40:25 -0700

    refactored js

--- index.js
+++ index.js
@@ -6,3 +6,39 @@ const images = [
     './assets/5.jpg',
     './assets/6.jpg'
 ]
+
+let currentIndex = 0
+
+let image = document.querySelector(".image");
+let previous = document.querySelector(".previous");
+let next = document.querySelector(".next")
+let buttons = document.querySelectorAll(".bubble");
+
+for (var i = 0; i < buttons.length; i++) {
+   let index = i;
+   buttons[i].addEventListener("click", function(e) {
+      currentIndex = index;
+      updateUI();
+   });
+}
+
+previous.addEventListener("click", function(e) {
+   currentIndex = currentIndex == 0 ? 5 : currentIndex - 1;
+   updateUI();
+});
+
+next.addEventListener("click", function(e) {
+   currentIndex = currentIndex == 5 ? 0 : currentIndex + 1;
+   updateUI();
+});
+
+function updateUI() {
+   image.setAttribute("src", images[currentIndex]);
+
+   for (var i = 0; i < buttons.length; i++) {
+      buttons[i].innerText = i == currentIndex ? "^" : "O"
+   }
+}
+
+updateUI();
+
